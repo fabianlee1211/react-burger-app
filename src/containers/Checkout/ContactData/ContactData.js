d046a548648f080ec8e25022191459857e973e36
Fabian Lee <chleefabian@gmail.com>
2018-08-28 21:01:01 +0800

    Added form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -22,7 +27,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -30,7 +40,14 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'ZIP Code'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 3,
+          maxLength: 5
+        },
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -38,7 +55,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
@@ -46,7 +68,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true
+        },
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -64,9 +91,14 @@ class ContactData extends Component {
   orderHandler = (event) => {
     event.preventDefault();
     this.setState({loading: true});
+    const formData = {};
+    for (let key in this.state.orderForm) {
+      formData[key] = this.state.orderForm[key].value; 
+    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
+      orderData: formData
     }
     axios.post('/orders.json', order)
       .then((response) => {
@@ -78,6 +110,37 @@ class ContactData extends Component {
     });
   }
 
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
+  inputChangedHandler= (event, inputIdentifier) => {
+    // Deep cloning objects
+    const updatedForm = {
+      ...this.state.orderForm
+    };
+    const updatedFormElement = {
+      ...updatedForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
+    updatedForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedForm});
+  }
+
   render() {
     const inputElements = [];
     for (let key in this.state.orderForm) {
@@ -86,15 +149,18 @@ class ContactData extends Component {
         config: this.state.orderForm[key]
       });
     }
-    console.log(inputElements);
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {inputElements.map((el) => (
           <Input
             key={el.id} 
             elementType={el.config.elementType} 
             elementConfig={el.config.elementConfig} 
-            value={el.config.value} />
+            value={el.config.value}
+            invalid={!el.config.valid}
+            shouldValidate={el.config.validation}
+            touched={el.config.touched} 
+            changed={(event) => this.inputChangedHandler(event, el.id)} />
         ))}
         <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
       </form>
